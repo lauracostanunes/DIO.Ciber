1c7284d07733af28aa2d058b19aeb571b9c1ecd3
Laura Nunes <lauracostanunes2@gmail.com>
2025-10-11 13:16:06 -0300

    Desafio de código 1

---
+++ Conceitos de autenticação e autorização.py
@@ -0,0 +1,15 @@
+entrada = input()
+def descrever_conceito(conceito):
+  if conceito == "Autenticação":
+    return "Verificação da identidade de um usuário"
+  
+  elif conceito == "Autorização":
+    return "Permissão para acessar recursos específicos"
+
+  elif conceito == "MFA":
+    return "Verificação usando múltiplos fatores de segurança"
+
+  elif conceito == "OAuth":
+    return "Padrão aberto para delegar acesso sem compartilhar senha"
+
+print(descrever_conceito(entrada))
